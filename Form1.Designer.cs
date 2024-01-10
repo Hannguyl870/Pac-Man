7f36da13e87587c3642f6dfdafa1b9bd1be01c1c
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2024-01-10 11:26:41 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Pac_Man
+{
+    partial class PacMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titlelable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titlelable
+            // 
+            this.titlelable.Location = new System.Drawing.Point(350, 181);
+            this.titlelable.Name = "titlelable";
+            this.titlelable.Size = new System.Drawing.Size(100, 23);
+            this.titlelable.TabIndex = 0;
+            this.titlelable.Text = "label1";
+            // 
+            // PacMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.titlelable);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PacMan";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PacMan_Paint);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titlelable;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pac_Man
+{
+    public partial class PacMan : Form
+    {
+        string mainmenue = "waiting";
+        public PacMan()
+        {
+            InitializeComponent();
+        }
+
+
+        private void PacMan_Paint(object sender, PaintEventArgs e)
+        {
+            if (mainmenue == "waiting")
+            {
+
+                //Score1lable.Text = "";
+
+                //Titlelable.Text = "SPACE RACER";
+                //Subtitlelable.Text = "Press Space to start or Esc to exit";
+
+            }
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+    }
+}
