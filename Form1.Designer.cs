388804b722c4a8f48750eafab42cd1c7f95f78b3
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2024-01-11 11:27:49 -0500

    aSF

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,38 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.titlelable = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.Titlelable = new System.Windows.Forms.Label();
+            this.Subtitlelable1 = new System.Windows.Forms.Label();
+            this.Subtitlelable2 = new System.Windows.Forms.Label();
+            this.pacmantimer = new System.Windows.Forms.Timer(this.components);
+            this.Scorelable = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // titlelable
+            // Titlelable
             // 
-            this.titlelable.Location = new System.Drawing.Point(350, 181);
-            this.titlelable.Name = "titlelable";
-            this.titlelable.Size = new System.Drawing.Size(100, 23);
-            this.titlelable.TabIndex = 0;
-            this.titlelable.Text = "label1";
+            this.Titlelable.BackColor = System.Drawing.Color.Transparent;
+            this.Titlelable.Font = new System.Drawing.Font("Algerian", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titlelable.Location = new System.Drawing.Point(8, -2);
+            this.Titlelable.Name = "Titlelable";
+            this.Titlelable.Size = new System.Drawing.Size(780, 101);
+            this.Titlelable.TabIndex = 0;
+            this.Titlelable.Text = "Titlelable";
+            this.Titlelable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Subtitlelable1
+            // 
+            this.Subtitlelable1.BackColor = System.Drawing.Color.Transparent;
+            this.Subtitlelable1.Font = new System.Drawing.Font("Algerian", 16F);
+            this.Subtitlelable1.Location = new System.Drawing.Point(10, 142);
+            this.Subtitlelable1.Name = "Subtitlelable1";
+            this.Subtitlelable1.Size = new System.Drawing.Size(783, 42);
+            this.Subtitlelable1.TabIndex = 1;
+            this.Subtitlelable1.Text = "Subtitlelable1";
+            // 
+            // Subtitlelable2
+            // 
+            this.Subtitlelable2.BackColor = System.Drawing.Color.Transparent;
+            this.Subtitlelable2.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Subtitlelable2.Location = new System.Drawing.Point(9, 214);
+            this.Subtitlelable2.Name = "Subtitlelable2";
+            this.Subtitlelable2.Size = new System.Drawing.Size(783, 42);
+            this.Subtitlelable2.TabIndex = 2;
+            this.Subtitlelable2.Text = "Subtitlelable2";
+            this.Subtitlelable2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // pacmantimer
+            // 
+            this.pacmantimer.Interval = 20;
+            this.pacmantimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Scorelable
+            // 
+            this.Scorelable.AutoSize = true;
+            this.Scorelable.BackColor = System.Drawing.Color.Transparent;
+            this.Scorelable.Font = new System.Drawing.Font("Algerian", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Scorelable.Location = new System.Drawing.Point(338, 9);
+            this.Scorelable.Name = "Scorelable";
+            this.Scorelable.Size = new System.Drawing.Size(114, 19);
+            this.Scorelable.TabIndex = 3;
+            this.Scorelable.Text = "Scorelable";
+            this.Scorelable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // PacMan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MidnightBlue;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.titlelable);
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Controls.Add(this.Scorelable);
+            this.Controls.Add(this.Subtitlelable2);
+            this.Controls.Add(this.Subtitlelable1);
+            this.Controls.Add(this.Titlelable);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PacMan";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.PacMan_Paint);
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label titlelable;
+        private System.Windows.Forms.Label Titlelable;
+        private System.Windows.Forms.Label Subtitlelable1;
+        private System.Windows.Forms.Label Subtitlelable2;
+        private System.Windows.Forms.Timer pacmantimer;
+        private System.Windows.Forms.Label Scorelable;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,40 +6,237 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 using System.Windows.Forms;
 
 namespace Pac_Man
 {
+   
     public partial class PacMan : Form
     {
+        int playerspeed = 4; 
         string mainmenue = "waiting";
+
+        SoundPlayer sp;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool dDown = false;
+        bool aDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool rightArrowDown = false;
+        bool leftArrowDown = false;
+
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush orangeBrush = new SolidBrush(Color.Orange);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+
+
+        Rectangle Wall1 = new Rectangle(260, 40, 5, 300);
+        Rectangle Wall2 = new Rectangle(50, 40, 5, 40);
+        Rectangle Wall3 = new Rectangle(600, 200, 5, 100); 
+        Rectangle Wall4 = new Rectangle(50, 40, 5, 40);
+        Rectangle Wall5 = new Rectangle(50, 40, 5, 40);
+
+
         public PacMan()
         {
             InitializeComponent();
         }
+        public void GameInitialize()
+        {
+            Graphics g = this.CreateGraphics();
 
+            Titlelable.Visible = false;
+            Subtitlelable1.Visible = false;
+            Subtitlelable2.Visible = false;
+            Titlelable.Text = "";
+            Subtitlelable1.Text = "";
+            Subtitlelable2.Text = "";
+            Scorelable.Text = "";
+
+            //player1score = 0;
+          
+            mainmenue = "running";
+
+        }
 
         private void PacMan_Paint(object sender, PaintEventArgs e)
         {
+            Graphics g = this.CreateGraphics();
             if (mainmenue == "waiting")
             {
 
                 //Score1lable.Text = "";
 
-                //Titlelable.Text = "SPACE RACER";
-                //Subtitlelable.Text = "Press Space to start or Esc to exit";
+                Titlelable.Text = "WELCOME PLAYER";
+                Subtitlelable1.Text = "               Press Space to start";
+                Subtitlelable2.Text = "            or Esc to exit";
+                Scorelable.Text = "";
+                Scorelable.Visible = false;
+            }
+            else if (mainmenue == "running")
+            {
+                pacmantimer.Enabled = true;
+                Titlelable.Visible = false;
+                Subtitlelable1.Visible = false;
+                Subtitlelable2.Visible = false;
+
+                Scorelable.Visible = true;
+
+                e.Graphics.FillRectangle(blackBrush, Wall1);
+                e.Graphics.FillRectangle(blackBrush, Wall2);
+                e.Graphics.FillRectangle(blackBrush, Wall3);
+                e.Graphics.FillRectangle(blackBrush, Wall4);
+                e.Graphics.FillRectangle(blackBrush, Wall5);
+
 
             }
         }
-        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-
-        }
+       
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
 
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+
+                case Keys.D:
+                    dDown = true;
+                    break;
+
+                case Keys.A:
+                    aDown = true;
+                    break;
+
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+
+                case Keys.Space:
+                    if (mainmenue == "waiting" || mainmenue == "playerwon" || mainmenue == "playerlost")
+                    {
+                        GameInitialize();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (mainmenue == "waiting" || mainmenue == "playerwon" || mainmenue == "playerlost")
+                    {
+                        this.Close();
+                    }
+                    break;
+            }
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //    if (upArrowDown == true && player.Y > Finishlinelable.Height - 3)
+            //    {
+            //        //player.Y -= playerSpeed;
+            //       
+
+            //    }
+            //    if (downArrowDown == true && player2.Y < this.Height - player2.Height)
+            //    {
+            //        player.Y += playerSpeed;
+            //      
+            //    }
+
+            //   if (rightArrowDown == true)
+            //    {
+            //         player.Y+= playerSpeed;
+            //    }
+
+            //    if (LeftArrowDown == true)
+            //    {
+            //         player.Y+= playerSpeed;
+            //    }
+
+            // if (player1.Y < this.Hight)
+            //{
+            //    player.Y = 400;
+            //   
+            //}
+           // if (player1score == 5)
+           // {
+                //Pacmantimer.Enabled = false;
+               // mainmenue = "playerwon";
+            //}
+
+            Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+
+                case Keys.D:
+                    dDown = true;
+                    break;
+
+                case Keys.A:
+                    aDown = true;
+                    break;
+
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+
+                case Keys.Space:
+                    if (mainmenue == "waiting" || mainmenue == "playerwon" || mainmenue == "playerlost")
+                    {
+                        GameInitialize();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (mainmenue == "waiting" || mainmenue == "playerwon" || mainmenue == "playerlost")
+                    {
+                        this.Close();
+                    }
+                    break;
+            }
+        }
     }
 }
